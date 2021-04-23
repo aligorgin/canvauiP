48db483f1b3370551e3a864b7cc2388e8f78698c
ali <aliwolfi18@gmail.com>
2021-04-24 01:01:11 +0430

    added header

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import {Header} from "~/ui/Header";
+
+export default function Home() {
+    return (
+        <div>
+            <div className='px-7 py-2 shadow-sm'>
+                <Header/>
+            </div>
+        </div>
+    )
+}
